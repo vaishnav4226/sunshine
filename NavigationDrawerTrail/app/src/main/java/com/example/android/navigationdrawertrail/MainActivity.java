dda2880437f97a82d0c32a7838ff01bf45fd6840
Sri Vaishnav <vaishnav4226@gmail.com>
2015-09-25 11:15:31 -0400

    first commit

---
+++ NavigationDrawerTrail/app/src/main/java/com/example/android/navigationdrawertrail/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.android.navigationdrawertrail;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void clicked(View view){
+        Intent intent = new Intent(this, NavigationDraw.class);
+        Toast.makeText(this, "It works!!", Toast.LENGTH_SHORT).show();
+        startActivity(intent);
+    }
+}
